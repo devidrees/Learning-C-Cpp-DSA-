9feeb306f51fbf7a1b97b6a536a12a830829e898
dev idrees <dev.idrees@gmail.com>
2024-09-22 18:12:48 +0530

    made changes to jsons to open a new terminal for execution. added array program.

--- Array/array.c
+++ Array/array.c
@@ -1,4 +1,18 @@
+#include <stdio.h>
+#define N 5
+
 void main(){
-    int arr[4]= {1,2,3,4};
-    //this is another cahnge in __FILE__
+    //int arr[N]= {1,2,3,4,5};
+
+    int arr[N];
+    for(int i=0;i<N;i++){
+        printf("Enter the input for index %d: ", i);
+        scanf("%d",&arr[i]);
+    }
+
+    printf("\nArray elements are: \n");
+    for(int i=0; i<N;i++){
+        printf("%d",arr[i]);
+    }
+    
 }
