db6fc2f3e7343cf01cfa591950f7f3ac4c1ec390
devidrees <dev.idrees@gmail.com>
2025-01-02 20:11:00 +0530

    update

--- Learning C++/Chapter 1 Introduction/hello.cpp
+++ Learning C++/Chapter 1 Introduction/hello.cpp
@@ -1,22 +1,2 @@
 
-#include <iostream>
-
-
-// to use the print command cout, we need std
-using namespace std;  
-
-// To use the format standard
-#include <format>
-
-using std::format;
-
-int main(){
-    
-    cout << "Hello Idrees!\n";
-    std::cout<<"Hello!\n";
-
-    return 0;
-
-}
-
-//Test making changes
+// C++ Learning by Idrees
