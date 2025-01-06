70330f622daee9ea493af6dc71e30ee650e44b2a
devidrees <dev.idrees@gmail.com>
2025-01-06 10:29:56 +0530

    update

--- Learning C++/Chapter 1 Introduction/04 Conditionals.cpp
+++ Learning C++/Chapter 1 Introduction/04 Conditionals.cpp
@@ -8,6 +8,7 @@ void main() {
 
 	int num = 11;
 // if else loop
+
 	if (num % 2 == 0) {
 		std::cout << "the number is even\n";
 	}
@@ -16,7 +17,7 @@ void main() {
 	}
 
 // if else ladder
-	// if else ladder
+
 	if (num>0) {
 		std::cout << "the number is natural\n";
 	}
@@ -32,6 +33,17 @@ void main() {
 		std::cout << std::format("Number is {}\n",i);
 	}
 
-
+// nested if else
+	if (num > 10) {
+		if (num < 20) {
+			std::cout << "Number is between 10 and 20.\n";
+		}
+		else {
+			std::cout << "Number is greater than 20.\n";
+		}
+	}
+	else {
+		std::cout << "Number is 10 or less.\n";
+	}
 
 }
