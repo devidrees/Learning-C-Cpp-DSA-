d815e5ff38e4a9e9cb69dc9d499c780507ad4bdf
devidrees <dev.idrees@gmail.com>
2025-01-09 17:33:47 +0530

    update

--- Learning C++/Chapter 2 Data Types/02 pointers and references.cpp
+++ Learning C++/Chapter 2 Data Types/02 pointers and references.cpp
@@ -10,26 +10,39 @@
 
 int main() {
 
-	int x{ 5 }; 
-	std::cout << std::format("x is : {} \n", x);
+	int x{ 52 }; // Value assignment of a variable
 
-	int* ptrx; // Pointer definition
+	int * ptrx; // Pointer definition
+	// memory allocated to a pointer. integer pointer because it points to an integer value.
 		
-	ptrx = &x;
+	ptrx = &x; // reference operator &
+	// & is the address of operator. the variable now holds the address of the interger variable named x
+	// returns address of an object suitable to hold a pointer
 
-	std::cout << "Address of x is : " << ptrx << "\n";
+	int q = * ptrx; // dereferencing the pointer. Dereference operator *
+	// Access the value pointed to, by the pointer
+	// it is same as to say r = x
+
+	std::cout << "x is : " << x << "\n";
+	std::cout << "Address of x, ptrx is : " << ptrx << "\n";
+	std::cout << "*ptrx, points to x is : " << *ptrx << "\n";
+	std::cout << "dereferencing, int q = *ptrx, and q is : " << q << "\n\n\n";
+
+//------------------------------------------------ references
+	// if we write int& r and assign a new value?
+	// Let's add a reference, int& reference to x
+
+	int& r = x;
+	std::cout << "Refer r to x, using this int& r = x; Then say r = 78, values change.\n";
+
+	r = 78;
 	
-	// ptr = 3; // won't work
+	std::cout << "x is : " << x << "\n";
+	std::cout << "Address of x, ptrx is : " << ptrx << "\n";
+	std::cout << "*ptrx is : " << *ptrx << "\n";
+	std::cout << "q is : " << q << "\n\n\n";
 
-	int y { 6 };
-
-	std::cout << "y is " << y << "\n";
-
-	int* ptry;
-
-	ptry = &y;
-
-	std::cout << "Address of y is : " << ptry << "\n";
+	// a refernce can't be changed.
 
 
 
