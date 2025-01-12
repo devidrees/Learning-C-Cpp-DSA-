6a49b3f2cd0f4fcdcfda3099bf662d7328169452
devidrees <dev.idrees@gmail.com>
2025-01-12 23:01:14 +0530

    updtae

--- Learning C++/Chapter 2 Data Types/02 pointers and references.cpp
+++ Learning C++/Chapter 2 Data Types/02 pointers and references.cpp
@@ -1,8 +1,17 @@
 
 // C++ Learning by Idrees
 
-//  name
-
+// A pointer is a variable that stores the memory address of another variable
+// normal variable that holds a data value
+// Pointer holds the address of a variable
+// Pointers provide a way to:
+// - Access and modify memory directly.
+// - Manipulate arrays in a more efficient way.
+// - Pass large structures (like arrays) to functions without 
+// copying the entire structure (pass-by-reference).
+// - Create complex data structures like linked lists, trees, and more.
+// 
+// 
 
 #include <iostream>
 #include <format>
@@ -12,14 +21,18 @@ int main() {
 
 	int x{ 52 }; // Value assignment of a variable
 
-	int * ptrx; // Pointer definition
+	int * ptrx; // Pointer definition/declaration
+	// ptrx is a pointer to an integer
 	// memory allocated to a pointer. integer pointer because it points to an integer value.
 		
-	ptrx = &x; // reference operator &
+	ptrx = &x; // ptrx now holds the address of x
+	// reference operator &
 	// & is the address of operator. the variable now holds the address of the interger variable named x
 	// returns address of an object suitable to hold a pointer
 
-	int q = * ptrx; // dereferencing the pointer. Dereference operator *
+	int q = * ptrx; // q is now 52 
+	// dereferencing the pointer. 
+	// Dereference operator *
 	// Access the value pointed to, by the pointer
 	// it is same as to say r = x
 
